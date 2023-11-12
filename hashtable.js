8b101f671f16a88ed1282384f320b32546c9e7fa
Ethan Szeto <ethan.szeto@gmail.com>
2023-11-12 01:43:09 -0500

    genuis

--- hashtable.js
+++ hashtable.js
@@ -6,8 +6,8 @@ class HashTable {
 
   //ring Zm hash function over strings
   hash(key) {
-    //abstracted
-    return hashAbs(key, this.max);
+    //abstracted, can use multiple hashes here
+    return hash3(key, this.max);
   }
 
   insert(key, value) {
@@ -22,22 +22,34 @@ class HashTable {
 
   find(key) {
     var hashID = this.hash(key);
-    var val = findRecur(this.table[hashID], key);
-
-    function findRecur(list, key) {
-      if (list.head.key == key) {
-        return list.head.value;
-      } else {
-        if (list.hasNext()) {
-          findRecur(list.tail, key);
-        } else {
-          return;
-        }
+    if (!this.table[hashID]) {
+      return;
+    } else {
+      var kvp = this.table[hashID].find(key);
+      if (!kvp) {
+        return;
       }
+      return kvp.value;
+    }
+  }
+
+  delete(key) {
+    var hashID = this.hash(key);
+    this.table[hashID] = this.table[hashID].delete(key);
+  }
+
+  increase(key) {
+    var hashID = this.hash(key);
+    var kvp = this.table[hashID].find(key);
+    if (kvp) {
+      kvp.value = kvp.value + 1;
     }
   }
 }
 
+/**
+ * Linked List of KVPs
+ */
 class LinkedList {
   constructor(head, tail) {
     this.head = head;
@@ -57,6 +69,48 @@ class LinkedList {
   //gets the value at the head
   getVal() {
     return this.head;
+  }
+
+  //not mutation, returns list with deleted thing
+  delete(key) {
+    return this.deleteRecur(this, key);
+  }
+
+  //delete helper method
+  deleteRecur(list, key) {
+    //remove off head
+    if (list.head.key == key) {
+      return list.tail;
+    }
+
+    //other
+    if (list.hasNext()) {
+      if (list.tail.head.key == key) {
+        return new LinkedList(list.head, list.tail.tail);
+      } else {
+        return new LinkedList(list.head, this.deleteRecur(list.tail, key));
+      }
+    } else {
+      return new LinkedList(list.head, list.tail);
+    }
+  }
+
+  //finds the KVP at said key. Returns undefined if not found
+  find(key) {
+    return this.findRecur(this, key);
+  }
+
+  //helper for finding
+  findRecur(list, key) {
+    if (list.head.key == key) {
+      return list.head;
+    } else {
+      if (list.hasNext()) {
+        return this.findRecur(list.tail, key);
+      } else {
+        return;
+      }
+    }
   }
 
   //converts this LinkedList into an array
@@ -80,7 +134,8 @@ class KVP {
   }
 }
 
-function hashAbs(key, max) {
+//my attempt at a hash function
+function hash1(key, max) {
   var keyArray = [];
 
   for (let i = 0; i < key.length; i++) {
@@ -131,7 +186,7 @@ function hashAbs(key, max) {
 
   function permute(array) {
     const permutation = [
-      11, 25, 0, 18, 19, 5, 17, 4, 16, 13, 22, 1, 6, 12, 20, 2, 24, 3, 23, 7,
+      11, 25, 26, 18, 19, 5, 17, 4, 16, 13, 22, 1, 6, 12, 20, 2, 24, 3, 23, 7,
       10, 9, 14, 21, 15, 14, 8,
     ];
     var output = [];
@@ -154,9 +209,29 @@ function hashAbs(key, max) {
       }
     }
     num = Math.floor(num);
-
     return num % m;
   }
+}
+
+//given hash function from someone online, modified to fit
+function hash2(str, m) {
+  let hash = 0;
+  for (let i = 0, len = str.length; i < len; i++) {
+    let chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  if (hash < 0) {
+    return (hash % m) + m - 1;
+  }
+  return hash % m;
+}
+
+function hash3(s, m) {
+  for (var i = 0, h = 0xdeadbeef; i < s.length; i++)
+    h = Math.imul(h ^ s.charCodeAt(i), 2654435761);
+  var output = (h ^ (h >>> 16)) >>> 0;
+  return output % m;
 }
 
 ////////////////////////////////////////////////////////////
@@ -165,19 +240,37 @@ function hashAbs(key, max) {
 
 window.addEventListener("load", () => {
   function testLinkedList() {
-    var list = new LinkedList(1, null);
-    var list1 = new LinkedList(2, list);
-    var list2 = new LinkedList(3, list1);
-    var list3 = new LinkedList(4, list2);
-    var list4 = new LinkedList(5, list3);
-    var list5 = new LinkedList(6, list4);
-    var list6 = new LinkedList(7, list5);
-    var list7 = list6.addToHead(8);
+    var kvp1 = new KVP("a", 1);
+    var kvp2 = new KVP("b", 2);
+    var kvp3 = new KVP("c", 3);
+    var kvp4 = new KVP("d", 4);
+    var kvp5 = new KVP("e", 5);
+    var kvp6 = new KVP("f", 6);
+    var kvp7 = new KVP("g", 7);
+    var kvp8 = new KVP("h", 8);
+    var kvp9 = new KVP("i", 9);
 
-    console.log(list4.getVal());
-    console.log(list3.hasNext());
-    console.log(list6.next());
-    console.log(list7);
+    var list1 = new LinkedList(kvp1, null);
+    var list2 = new LinkedList(kvp2, list1);
+    var list3 = new LinkedList(kvp3, list2);
+    var list4 = new LinkedList(kvp4, list3);
+    var list5 = new LinkedList(kvp5, list4);
+    var list6 = new LinkedList(kvp6, list5);
+    var list7 = new LinkedList(kvp7, list6);
+    var list8 = new LinkedList(kvp8, list7);
+    var list9 = new LinkedList(kvp9, list8);
+
+    console.log(list9.toArray());
+
+    console.log(list9.find("f"));
+
+    console.log(list9.find("x"));
+
+    console.log(list9.delete("h").toArray());
+
+    console.log(list9.delete("i").toArray());
+
+    console.log(list9.delete("x").toArray());
   }
 
   function testHash() {
@@ -198,13 +291,54 @@ window.addEventListener("load", () => {
 
   function testHashTable() {
     var hashT = new HashTable(10);
-    hashT.insert("myname", 20);
-    hashT.insert("myname", 21);
-    hashT.insert("myname", 22);
+
+    var str =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis eu volutpat odio facilisis mauris sit amet massa. Amet facilisis magna etiam tempor orci eu lobortis elementum. Orci porta non pulvinar neque laoreet suspendisse interdum. Ipsum nunc aliquet bibendum enim facilisis gravida. Sodales ut etiam sit amet nisl purus in mollis. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Fames ac turpis egestas sed tempus urna et. Imperdiet proin fermentum leo vel orci porta non. Varius vel pharetra vel turpis nunc. Dui ut ornare lectus sit. Non odio euismod lacinia at quis risus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Ut faucibus pulvinar elementum integer enim neque volutpat ac. Lacus vestibulum sed arcu non odio euismod lacinia at. Semper auctor neque vitae tempus quam. Feugiat nibh sed pulvinar proin gravida hendrerit. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Tortor condimentum lacinia quis vel eros donec. Auctor neque vitae tempus quam pellentesque nec nam. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Amet commodo nulla facilisi nullam. Amet commodo nulla facilisi nullam vehicula ipsum. Fermentum et sollicitudin ac orci phasellus. Ac turpis egestas integer eget aliquet. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Auctor elit sed vulputate mi sit amet. Cras semper auctor neque vitae tempus. Tristique et egestas quis ipsum. Dictum varius duis at consectetur lorem donec massa sapien. Nec nam aliquam sem et. Ornare arcu dui vivamus arcu felis bibendum ut tristique et. Arcu odio ut sem nulla. Scelerisque eleifend donec pretium vulputate sapien. Nisl purus in mollis nunc sed id semper risus in. Ipsum dolor sit amet consectetur adipiscing. Adipiscing at in tellus integer feugiat scelerisque varius morbi enim. Vel facilisis volutpat est velit. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Accumsan tortor posuere ac ut. Convallis aenean et tortor at risus viverra adipiscing. Tortor condimentum lacinia quis vel eros donec. Faucibus ornare suspendisse sed nisi lacus. Risus pretium quam vulputate dignissim suspendisse in est. Convallis a cras semper auctor. Convallis a cras semper auctor neque vitae tempus quam. Amet consectetur adipiscing elit ut aliquam. Consectetur libero id faucibus nisl tincidunt. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Justo nec ultrices dui sapien eget mi. Pulvinar sapien et ligula ullamcorper malesuada proin libero. Quis viverra nibh cras pulvinar. Euismod lacinia at quis risus sed. Tincidunt tortor aliquam nulla facilisi cras fermentum. Risus nec feugiat in fermentum posuere urna nec. Fringilla ut morbi tincidunt augue interdum. Aenean sed adipiscing diam donec adipiscing tristique. Scelerisque eu ultrices vitae auctor eu augue. Augue ut lectus arcu bibendum at varius vel pharetra. Pellentesque elit eget gravida cum sociis natoque penatibus. Scelerisque eu ultrices vitae auctor eu augue ut lectus. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Et sollicitudin ac orci phasellus egestas tellus rutrum. Iaculis nunc sed augue lacus";
+
+    var strArr = str.split(" ");
+
+    /*
+    //for positions of each word (EXTRA CREDIT)
+    for (let i = 0; i < strArr.length; i++) {
+      hashT.insert(strArr[i], i);
+    }
+    */
+
+    //for count of each word
+    for (let i = 0; i < strArr.length; i++) {
+      //if key doesn't exist
+      if (!hashT.find(strArr[i])) {
+        hashT.insert(strArr[i], 1);
+      } else {
+        //key alr exists
+        hashT.increase(strArr[i]);
+      }
+    }
 
     console.log(hashT.table);
+
+    var collisions = [];
+    for (let i = 0; i < hashT.table.length; i++) {
+      if (hashT.table[i]) {
+        console.log(hashT.table[i].toArray());
+        collisions.push(hashT.table[i].toArray().length);
+      }
+    }
+
+    console.log("------------------------------------------------");
+    console.log(hashT.find("sapien"));
+    console.log(hashT.table[hashT.hash("nunc")].toArray());
+    hashT.delete("nunc");
+    hashT.delete("nam");
+    hashT.delete("tempor");
+    hashT.delete("x");
+    console.log(hashT.table[hashT.hash("nunc")].toArray());
+    console.log("------------------------------------------------");
+
+    //produce histogram based off this one
+    console.log(collisions);
   }
 
   //testLinkedList();
-  testHashTable();
+  //testHashTable();
 });
---
+++ ht.ctrl.js
@@ -0,0 +1,83 @@
+var canvas;
+var ctx;
+
+window.addEventListener("load", () => {
+  canvas = document.getElementById("histogram");
+  ctx = canvas.getContext("2d");
+  document
+    .getElementById("generate-histogram")
+    .addEventListener("click", () => {
+      var text = document.getElementById("text").value;
+      var maxHash = document.getElementById("max-hash").value;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      var hashT = new HashTable(maxHash);
+
+      text = text.split(" ");
+
+      for (let i = 0; i < text.length; i++) {
+        //if key doesn't exist
+        if (!hashT.find(text[i])) {
+          hashT.insert(text[i], 1);
+        } else {
+          //key alr exists
+          hashT.increase(text[i]);
+        }
+      }
+
+      var collisions = [];
+      for (let i = 0; i < hashT.table.length; i++) {
+        if (hashT.table[i]) {
+          collisions.push(hashT.table[i].toArray().length);
+        } else {
+          collisions.push(0);
+        }
+      }
+
+      const barWidth = canvas.width / maxHash;
+
+      var maxHeightNum1 = Math.max(...collisions);
+      var maxHeightDenom1 = maxHeightNum1 * (8 / 7); //87.5% the way total
+
+      var maxHeightDenom2 = canvas.height;
+
+      var denomRatio = maxHeightDenom2 / maxHeightDenom1;
+
+      for (let i = 0; i < collisions.length; i++) {
+        collisions[i] = collisions[i] * denomRatio;
+      }
+
+      //collisions becomes height of each bar
+
+      for (let i = 0; i < collisions.length; i++) {
+        var xPos = i * barWidth;
+        var yPos = canvas.height - collisions[i];
+        var bar = new Rect(
+          xPos,
+          yPos,
+          Math.ceil(barWidth),
+          collisions[i],
+          "#000000"
+        );
+        bar.draw();
+      }
+
+      //need to draw some scale / tickmarks -- I think 4 is enough.
+    });
+});
+
+class Rect {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
